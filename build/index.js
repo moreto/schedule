c8e88d61924b0a8a3e20ca3d1305bebedc956179
Marcelo Moreto <mmoreto@gmail.com>
2021-05-20 18:55:15 -0300

    schedule init

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+var schedule = require('node-schedule');
+var jobOne = schedule.scheduleJob('44 * * * *', function () {
+    console.log('Job One - Execute a cron job when the minute is 44!');
+});
+var jobTwo = schedule.scheduleJob('*/1 * * * *', function () {
+    console.log('Job Two - Execute a cron job every 1 Minutes!');
+});
